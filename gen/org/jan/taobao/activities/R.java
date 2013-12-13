32971f76a5300ddad3c8b65aab572d1de1b740d8
wshh_jan <sunjun2006311@163.com>
2013-12-13 17:19:49 +0800

    完成登陆的ui界面

--- gen/org/jan/taobao/activities/R.java
+++ gen/org/jan/taobao/activities/R.java
@@ -462,16 +462,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int exit_btn=0x7f090004;
-        public static final int indentifyingcode=0x7f090002;
-        public static final int register_btn=0x7f090003;
+        public static final int action_settings=0x7f090006;
+        public static final int exit_btn=0x7f090003;
+        public static final int indentifyingcode=0x7f090005;
+        public static final int register_btn=0x7f090002;
+        public static final int textView1=0x7f090004;
         public static final int user_r_name=0x7f090000;
         public static final int user_r_password=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int register_layout=0x7f030001;
+        public static final int login_layout=0x7f030001;
+        public static final int register_layout=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -482,6 +484,8 @@ public final class R {
         public static final int exit_button=0x7f060008;
         public static final int hello_world=0x7f060002;
         public static final int indentifyingcode=0x7f060006;
+        public static final int login_title=0x7f06000d;
+        public static final int login_txt=0x7f06000c;
         public static final int r_hint_1_txt=0x7f060009;
         public static final int r_hint_2_txt=0x7f06000a;
         public static final int r_hint_3_txt=0x7f06000b;
@@ -506,5 +510,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int my_style_1=0x7f070002;
     }
 }
--- src/org/jan/taobao/activities/RegisterActivity.java
+++ src/org/jan/taobao/activities/RegisterActivity.java
@@ -1,30 +1,42 @@
 package org.jan.taobao.activities;
 
+import java.io.FileOutputStream;
+
+import org.jan.taobao.entity.TaobaoUser;
+import org.jan.taobao.utils.SaxTaobaoUtils;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class RegisterActivity extends Activity implements OnClickListener{
+public class RegisterActivity extends Activity implements OnClickListener {
 
 	private EditText user_name;
 	private EditText user_pwd;
 	private EditText indentifyCode;
 	private Button register_btn;
 	private Button exitBtn;
+	private TaobaoUser user;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_LEFT_ICON);
 		setContentView(R.layout.register_layout);
+		getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+				android.R.drawable.ic_menu_more);
 		inieView();
-		
+
 	}
-	
-	private void inieView(){
-		user_name=(EditText) findViewById(R.id.user_r_name);
-		user_pwd= (EditText) findViewById(R.id.user_r_password);
+
+	private void inieView() {
+		user_name = (EditText) findViewById(R.id.user_r_name);
+		user_pwd = (EditText) findViewById(R.id.user_r_password);
 		indentifyCode = (EditText) findViewById(R.id.indentifyingcode);
 		register_btn = (Button) findViewById(R.id.register_btn);
 		exitBtn = (Button) findViewById(R.id.exit_btn);
@@ -36,11 +48,70 @@ public class RegisterActivity extends Activity implements OnClickListener{
 	public void onClick(View source) {
 		switch (source.getId()) {
 		case R.id.register_btn:
-			//TODO 注册功能
+			// TODO 注册功能
+			if (validate()) {
+				register();
+				Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
 			break;
-
+		case R.id.exit_btn:
+			finish();
+			break;
 		default:
 			break;
 		}
+	}
+
+	/**
+	 * 验证用户输入是否合法
+	 */
+	private boolean validate() {
+		String name = user_name.getText().toString().trim();
+		if ("".equals(name)) {
+			user_name.setText("");
+			user_name.setHint("用户名不能为空");
+			user_name.setHintTextColor(getResources().getColor(R.color.red));
+			return false;
+		}
+		String password = user_pwd.getText().toString().trim();
+		if ("".equals(password)) {
+			user_pwd.setText("");
+			user_pwd.setHint("密码不能为空");
+			user_pwd.setHintTextColor(getResources().getColor(R.color.red));
+			return false;
+		}
+		String identifyCode = indentifyCode.getText().toString().trim();
+		if ("".equals(identifyCode)) {
+			indentifyCode.setText("");
+			indentifyCode.setHint("请填写...");
+			indentifyCode
+					.setHintTextColor(getResources().getColor(R.color.red));
+			return false;
+		}
+		return true;
+	}
+	/**
+	 * 用户注册服务
+	 * @return 是否注册成功
+	 */
+	private boolean register(){
+		String userName=user_name.getText().toString();
+		String pwd = user_pwd.getText().toString();
+		user = new TaobaoUser();
+		user.setUserName(userName);
+		user.setUserPwd(pwd);
+		String userInfo=null;
+		try {
+			userInfo=SaxTaobaoUtils.parsetoXml(user);
+			FileOutputStream fos=openFileOutput("taobao_user.xml", MODE_PRIVATE);
+			fos.write(userInfo.getBytes("utf-8"));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 }
